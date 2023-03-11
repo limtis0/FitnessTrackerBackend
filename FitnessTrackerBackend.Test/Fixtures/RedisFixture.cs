e3f7a08bdb84cb7e694f9493770c582e20e584e1
Vladimir <limtis2109@gmail.com>
2023-03-11 16:48:29 +0100

    Separate redis image for tests

--- FitnessTrackerBackend.Test/Fixtures/RedisFixture.cs
+++ FitnessTrackerBackend.Test/Fixtures/RedisFixture.cs
@@ -1,27 +1,80 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Docker.DotNet.Models;
+using Docker.DotNet;
 using StackExchange.Redis;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FitnessTrackerBackend.Test.Fixtures
 {
     public class RedisFixture : IDisposable
     {
+        private readonly DockerClient _dockerClient;
+        private readonly string _containerId;
+        private readonly ConnectionMultiplexer _redis;
+
         public RedisFixture()
         {
-            // Set up the Redis instance
+            // Create a Docker client
+            _dockerClient = new DockerClientConfiguration(new Uri("npipe://./pipe/docker_engine")).CreateClient();
+
+            _containerId = StartRedisContainer("6380").ID;
+
+            _dockerClient.Containers.StartContainerAsync(_containerId, null).GetAwaiter().GetResult();
+
+            // Connect to the Redis container
+            _redis = ConnectionMultiplexer.Connect("localhost:6380");
+
             var services = new ServiceCollection();
             services.AddSingleton<IConnectionMultiplexer>(provider =>
             {
-                var configuration = ConfigurationOptions.Parse("localhost:6379");
+                var configuration = ConfigurationOptions.Parse("localhost:6380");
                 return ConnectionMultiplexer.Connect(configuration);
             });
             ServiceProvider = services.BuildServiceProvider();
         }
 
+        private CreateContainerResponse StartRedisContainer(string hostPort)
+        {
+            // Pull the Redis image
+            _dockerClient.Images.CreateImageAsync(new ImagesCreateParameters
+            {
+                FromImage = "redis",
+                Tag = "latest"
+            }, null, new Progress<JSONMessage>()).GetAwaiter().GetResult();
+
+            // Start a Redis container
+            var createContainerResponse = _dockerClient.Containers.CreateContainerAsync(new CreateContainerParameters
+            {
+                Image = "redis:latest",
+                Name = "redis.test",
+                HostConfig = new HostConfig
+                {
+                    PortBindings = new Dictionary<string, IList<PortBinding>>
+                {
+                    {
+                        "6379/tcp",
+                        new List<PortBinding>
+                        {
+                            new PortBinding
+                            {
+                                HostPort = hostPort
+                            }
+                        }
+                    }
+                }
+                }
+            }).GetAwaiter().GetResult();
+
+            return createContainerResponse;
+        }
 
         public IServiceProvider ServiceProvider { get; private set; }
 
         public void Dispose()
         {
+            // Stop and remove the Redis container
+            _dockerClient.Containers.StopContainerAsync(_containerId, new ContainerStopParameters()).GetAwaiter().GetResult();
+            _dockerClient.Containers.RemoveContainerAsync(_containerId, new ContainerRemoveParameters()).GetAwaiter().GetResult();
+
             GC.SuppressFinalize(this);
         }
     }
