f5dae495bc4f0c582ad67869e941c7a18b7764cb
Vladimir <limtis2109@gmail.com>
2023-03-13 19:08:11 +0100

    Added two JWT-Related methods to IRedisUserService

--- FitnessTrackerBackend/Services/Users/IRedisUsersService.cs
+++ FitnessTrackerBackend/Services/Users/IRedisUsersService.cs
@@ -6,12 +6,16 @@ namespace FitnessTrackerBackend.Services.Authentication
     {
         Task<UserModel?> RegisterUserAsync(UserRegistrationModel registration);
         Task<UserModel?> LoginUserAsync(UserLoginModel login);
+
         string GenerateUserJWTToken(UserModel user);
         Task<UserModel?> GetUserByJWTToken(string token);
+
         Task<UserModel?> GetUserByIdAsync(string userId);
         Task<UserModel?> GetUserByEmailAsync(string email);
         Task<UserModel?> GetUserByUsernameAsync(string username);
+
         Task<bool> RemoveUserAsync(string userId);
+
         Task<string> GetUserCount();
     }
 }
